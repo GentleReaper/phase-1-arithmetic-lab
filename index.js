e9590c95f9f33bc248bdab63c3f78f7e31fd8a8c
GentleReaper <cyraxmuia@gmail.com>
2025-02-28 20:19:50 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1= 31;
+let num2= 2;
+let multiply= 62;
+console.log(`${num1} *${num2}=${multiply}`);
+
+let random=Math.floor(Math.random()*100)+1;
+console.log(random);
+let num3= 24;
+let num4= 5;
+let mod=4;
+console.log(`${num3} %${num4}=${mod}`);
+
+let max= 20;
+console.log(max);
